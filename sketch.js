23c2977afe743b223b032eaf6aa12a32cd8ddd04
배서연 <dexrchive@gmail.com>
2026-04-16 17:08:57 +0900

    hi

--- sketch.js
+++ sketch.js
@@ -6,5 +6,11 @@ function draw() {
   background(220);
 }
 
-// 0416 써져라!!!!
-abcdgi
+function draw(){
+  background(20, 24, 30);
+  fill(255, 0, 0);
+  text("Hello, World!", 100, 100);
+  rect(200, 200, 50, 50);
+  rect(350, 300, 100, 50);
+  rect(500, 200, 100, 50);
+}
